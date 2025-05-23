69afcfa23761cbd13917d93a09bb46dde9aa66f6
mieubrisse <mieubrisse@gmail.com>
2025-05-23 14:18:01 -0300

    Add list-files and preview commands

---
+++ list-files.sh
@@ -0,0 +1,67 @@
+fd_base_cmd="fd --follow --hidden --color=always"
+
+# !!! NOTE !!!! order is important!!
+# fzf gives higher weight to lines earlier in the input, so we put most relevant things first
+
+if [ "${PWD}" = "${HOME}" ]; then
+    # Skip several directories in home that contain a bunch of garbage
+    ${fd_base_cmd} --strip-cwd-prefix \
+        -E 'Applications' \
+        -E 'Library' \
+        -E '.pyenv' \
+        -E '.jenv' \
+        -E '.nvm' \
+        -E 'go' \
+        -E 'venvs' \
+        -E '.cursor' \
+        -E '.docker' \
+        -E '.vscode' \
+        -E '.cache' \
+        -E '.gradle' \
+        -E '.zsh_sessions' \
+        .
+else
+    ${fd_base_cmd} --strip-cwd-prefix .
+fi
+
+echo 'HOME'   # HOME
+echo '..'     # Parent directory
+
+# If we're not in the home directory, include stuff in the home directory
+if [ "${PWD}" != "${HOME}" ]; then
+    # Skip the Applications and Library in the home directory; they contain a bunch of garbage
+    ${fd_base_cmd} \
+        -E 'Applications' \
+        -E 'Library' \
+        -E '.pyenv' \
+        -E '.jenv' \
+        -E '.nvm' \
+        -E 'go' \
+        -E 'venvs' \
+        -E '.cursor' \
+        -E '.docker' \
+        -E '.vscode' \
+        -E '.cache' \
+        -E '.gradle' \
+        -E '.zsh_sessions' \
+        . \
+        "${HOME}"
+fi
+
+echo '/tmp/'  # /tmp
+
+echo '/'      # Root
+${fd_base_cmd} --exact-depth 1 . / # Show one level of root
+
+# Add back .pyenv and .jenv just in case the user wants to 'cd' to them
+echo "${HOME}/.pyenv"
+echo "${HOME}/.jenv"
+echo "${HOME}/.nvm"
+echo "${HOME}/go"
+echo "${HOME}/venvs"
+echo "${HOME}/.cursor"
+echo "${HOME}/.docker"
+echo "${HOME}/.vscode"
+echo "${HOME}/.cache"
+echo "${HOME}/.gradle"
+echo "${HOME}/.zsh_sessions"
---
+++ preview.sh
@@ -0,0 +1,20 @@
+ls_base_cmd='ls --color=always'
+
+case "${1}" in
+    HOME)
+        ${ls_base_cmd} "${HOME}"
+        ;;
+    *)
+        case $(file -b --mime-type "${1}" ) in 
+            text/*) 
+                bat --color=always "${1}"
+                ;; 
+            inode/directory) 
+                ${ls_base_cmd} "${1}"
+                ;; 
+            image/*) 
+                tiv -w 100 -h 100 "${1}"
+                ;; 
+        esac
+        ;;
+esac
