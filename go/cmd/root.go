fba495f1cd6435cb7c92c15a603a95b6e0a5f545
Kevin Today <1142185+mieubrisse@users.noreply.github.com>
2026-02-23 15:28:19 -0300

    Initialize Go module with Cobra skeleton

---
+++ go/cmd/root.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "cmdk",
+	Short: "Terminal file navigator -- Cmd-K for your shell",
+	Long:  "cmdk brings the Cmd-K 'access anything' experience to the terminal.\nUsage: eval \"$(cmdk init)\" in your shell rc file.",
+}
+
+var (
+	flagPWDOnly bool
+	flagSubdirs bool
+)
+
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Launch the file navigator (called by shell function)",
+	RunE:  runCmdk,
+}
+
+func init() {
+	runCmd.Flags().BoolVarP(&flagPWDOnly, "pwd-only", "o", false, "List current directory contents only (depth 1)")
+	runCmd.Flags().BoolVarP(&flagSubdirs, "subdirs", "s", false, "List current directory and recurse into subdirectories")
+	rootCmd.AddCommand(runCmd)
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func runCmdk(command *cobra.Command, args []string) error {
+	// TODO: will be implemented in subsequent tasks
+	return nil
+}
